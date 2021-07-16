d666cad56896e514d837806545a5a57e6556d935
Lennon <MDigital20@yahoo.com>
2021-07-16 17:55:38 -0300

    Developed person delete operation

--- src/main/java/one/digitalInnovation/personApi/controller/PersonController.java
+++ src/main/java/one/digitalInnovation/personApi/controller/PersonController.java
@@ -39,5 +39,12 @@ public class PersonController {
     public PersonDTO findById(@PathVariable Long id) throws PersonNotFoundException {
         return personService.findById(id);
     }
+
+    @DeleteMapping ("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteById(@PathVariable Long id) throws PersonNotFoundException {
+        personService.delete(id);
+    }
+
 }
 
